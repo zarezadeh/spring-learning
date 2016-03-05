4a9b1d6568921f4b957b9adaa71550390bdae717
Meysam Zarezadeh <zarezadeh@gmail.com>
2016-03-05 21:27:12 +0330

    initial commit

---
+++ src/main/java/com/github/zarezadeh/qa/QAApplication.java
@@ -0,0 +1,10 @@
+package com.github.zarezadeh.qa;
+
+/**
+ * @author Meysam Zarezadeh <zarezadeh@gmail.com>
+ */
+public class QAApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World from QA!");
+    }
+}
